acb6b99ad6df4e318ab2a94c7aab8487f965e8c4
Onur Baykal <theaob@gmail.com>
2012-02-16 14:54:16 +0200

    First commit for Sencha CSS & JS files

---
+++ js/etumobil.js
@@ -0,0 +1,13 @@
+new Ext.Application({
+	name: "etumobil",
+    launch: function() {
+		var app = this;
+	
+		var viewport = new Ext.Panel({
+            fullscreen: true,
+            layout: 'card',
+        });
+		
+		
+    }
+});
